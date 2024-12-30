7c9b60e6d3c8e752f04ba51e03f2be7fcf82974a
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-12-30 17:09:47 +0100

    feat: bg + login component

--- Front/CartodexTCG/src/Routes/AppRoutes.jsx
+++ Front/CartodexTCG/src/Routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter , Route , Routes } from "react-router-dom";
+import { Login } from "../Components/Login/Login.jsx";
 
 export const AppRoutes = () => {
     return (
@@ -6,6 +7,7 @@ export const AppRoutes = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<p>It"s works</p>}/>
+                    <Route path="/login" element={<Login/>}/>
                     <Route path="*" element={<p>Erreur 404</p>}/>
                 </Routes>
             </BrowserRouter>
