969e83aaa52f78b3145e305c20eabf64eaeb79ce
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-12-31 16:31:34 +0100

    feat: home label + card pokemon

---
+++ Front/CartodexTCG/src/Components/Home/Home.style.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const DivHomeContent = styled.div`
+    display: flex;
+    align-items: center;
+    height: 80%;
+`;
+
+export const LabelHome = styled.label`
+    display: flex;
+    align-items: center;
+    font-size: xx-large;
+    height: 20%;
+`;
+
+export const DivHome = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+`;
--- Front/CartodexTCG/src/Routes/AppRoutes.jsx
+++ Front/CartodexTCG/src/Routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import { Footer } from "../Components/Footer/Footer.jsx";
 import { DivContent } from "./AppRoutes.style.jsx";
 import { Register } from "../Components/Register/Register.jsx";
 import { PokemonSerieList } from "../Components/PokemonSerieList/PokemonSerieList.jsx";
+import { Home } from "../Components/Home/Home.jsx";
 
 export const AppRoutes = () => {
     return (
@@ -16,6 +17,7 @@ export const AppRoutes = () => {
                         <Route path="/" element={<p>It"s works</p>}/>
                         <Route path="/login" element={<Login/>}/>
                         <Route path="/register" element={<Register/>}/>
+                        <Route path="/home" element={<Home/>}/>
                         <Route path="/pokemon" element={<PokemonSerieList/>}/>
                         <Route path="*" element={<p>Erreur 404</p>}/>
                     </Routes>
