4ea0519ca570d5cf8360ea3bcb0c81012dfc4ddd
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-12-31 16:49:40 +0100

    feat: init back maven spring

---
+++ Back/cartodexTCG/src/test/java/fr/cartodexTCG/cartodexTCG/CartodexTcgApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cartodexTCG.cartodexTCG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartodexTcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
