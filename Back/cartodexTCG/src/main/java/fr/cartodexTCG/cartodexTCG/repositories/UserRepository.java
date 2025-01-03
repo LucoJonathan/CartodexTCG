09eaa25680a68ae55f369fa7c8bcb4d913e4080a
Jonathan Luco <160269692+LucoJonathan@users.noreply.github.com>
2025-01-03 16:10:06 +0100

    Delete Back directory

--- Back/cartodexTCG/src/main/java/fr/cartodexTCG/cartodexTCG/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.cartodexTCG.cartodexTCG.repositories;
-
-import fr.cartodexTCG.cartodexTCG.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
