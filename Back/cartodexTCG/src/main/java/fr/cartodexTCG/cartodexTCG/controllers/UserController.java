751983b42c1ed00d99850fdfd54e5d24e4ad29b7
LucoJonathan <jonathan.luco44@gmail.com>
2025-01-03 13:14:19 +0100

    feat: UserController getAll work In progress é

--- Back/cartodexTCG/src/main/java/fr/cartodexTCG/cartodexTCG/controllers/UserController.java
+++ Back/cartodexTCG/src/main/java/fr/cartodexTCG/cartodexTCG/controllers/UserController.java
@@ -4,6 +4,8 @@ import fr.cartodexTCG.cartodexTCG.dtos.UserDto;
 import fr.cartodexTCG.cartodexTCG.services.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,11 @@ public class UserController {
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public ResponseEntity<UserDto> getUserById(@PathVariable int id) {
         return ResponseEntity.ok(service.getById(id));
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, params = {"page", "size"})
+    public ResponseEntity<Page<UserDto>> getAllUsersByPage(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 
     @DeleteMapping(value = "/{id}")
