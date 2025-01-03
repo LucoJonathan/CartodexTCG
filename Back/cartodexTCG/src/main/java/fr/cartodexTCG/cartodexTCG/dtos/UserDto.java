fabcce5fa41e1f1cc3215f3c2a546ac29a3a0fce
LucoJonathan <jonathan.luco44@gmail.com>
2025-01-03 11:11:53 +0100

    feat: UserDto work in progress Ã

--- Back/cartodexTCG/src/main/java/fr/cartodexTCG/cartodexTCG/dtos/UserDto.java
+++ Back/cartodexTCG/src/main/java/fr/cartodexTCG/cartodexTCG/dtos/UserDto.java
@@ -1,4 +1,10 @@
 package fr.cartodexTCG.cartodexTCG.dtos;
 
-public class UserDto {
+import fr.cartodexTCG.cartodexTCG.generic.dto.DtoGeneric;
+
+public class UserDto extends DtoGeneric {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
